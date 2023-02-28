5d213cf2a56216383f24410e239e0ae2a66e0733
matocool <83129369+matocool@users.noreply.github.com>
2023-02-28 13:56:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -216,7 +216,7 @@ def main(_user, _passwd, min_1, max_1):
 
 # 获取时间戳
 def get_time():
-    url = 'https://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
+    url = 'https://apps.game.qq.com/CommArticle/app/reg/gdate.php'
     response = requests.get(url, headers=headers).json()
     t = response['currentTime2']
     return t
