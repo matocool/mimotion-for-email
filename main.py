cf2b3e72816483c02e0bca45492e5d3f156b6b2e
matocool <83129369+matocool@users.noreply.github.com>
2023-02-28 14:11:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ def main(_user, _passwd, min_1, max_1):
 
     response = requests.post(url, data=data, headers=head).json()
     # print(response)
-    result = f"[{now}]\n\n\n\n账号：{user[:3]}****{user[7:]} 修改步数（{step}）\\[" + response['message'] + "]\n\n\n"
+    result = f"[{now}]\n\n{user[:3]}****{user[7:]} 改步（{step}）\\[" + response['message'] + "]\n\n"
     print(result)
     return result
 
