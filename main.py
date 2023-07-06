c45634af5b48ab997b4b7cecefdd58165e1c7d7e
matocool <83129369+matocool@users.noreply.github.com>
2023-07-06 23:51:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ def getBeijinTime():
         print(min_ratio)
         print(max_ratio)
         max_ratio = int(hour)
-        min_1 = 4500 * min_ratio
-        max_1 = 3500 * max_ratio
+        min_1 = 3300 * min_ratio
+        max_1 = 3300 * max_ratio
         min_1 = int(K * min_1)
         max_1 = int(K * max_1)
         print("天气系数是")
